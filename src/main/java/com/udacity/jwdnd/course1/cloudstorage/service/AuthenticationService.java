208023dcebdcffe3123da574034d9a92214e26dc
lapivoinerouge <wioladys@gmail.com>
2020-07-25 21:29:55 +0200

    code refactor + comments

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/service/AuthenticationService.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/service/AuthenticationService.java
@@ -22,19 +22,24 @@ public class AuthenticationService implements AuthenticationProvider {
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        // credentials from login page
         String username = authentication.getName();
         String password = authentication.getCredentials().toString();
 
         User user = mapper.getUser(username);
+
+        // check if user exists and compare passwords
         if (user != null) {
             String encodedSalt = user.getSalt();
             String hashedPassword = hashService.getHashedValue(password, encodedSalt);
             if (user.getPassword().equals(hashedPassword)) {
+                // last argument represents list of permission user was granted
                 return new UsernamePasswordAuthenticationToken(username, password, new ArrayList<>());
             }
         } return null;
     }
 
+    // info for Spring which authorization schemes this class can handle
     @Override
     public boolean supports(Class<?> authentication) {
         return authentication.equals(UsernamePasswordAuthenticationToken.class);
