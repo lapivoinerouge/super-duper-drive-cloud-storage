648ebad958bfff71f5e48704c97901a6758f4d3a
lapivoinerouge <wioladys@gmail.com>
2020-07-25 21:11:33 +0200

    add UserService

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/service/UserService.java
@@ -0,0 +1,32 @@
+package com.udacity.jwdnd.course1.cloudstorage.service;
+
+import com.udacity.jwdnd.course1.cloudstorage.entity.User;
+import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+@Service
+public class UserService {
+    private final UserMapper mapper;
+    private final HashService hashService;
+
+    public UserService(UserMapper mapper, HashService hashService) {
+        this.mapper = mapper;
+        this.hashService = hashService;
+    }
+
+    public boolean isUsernameAvailable(String username) {
+        return mapper.getUser(username) == null;
+    }
+
+    public int createUser(User user) {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        String encodedSalt = Base64.getEncoder().encodeToString(salt);
+        String hashedPassword = hashService.getHashedValue(user.getPassword(), encodedSalt);
+        return mapper.insertUser(new User(null, user.getUsername(), user.getPassword(), encodedSalt, user.getFirstName(), user.getLastName()));
+    }
+}
