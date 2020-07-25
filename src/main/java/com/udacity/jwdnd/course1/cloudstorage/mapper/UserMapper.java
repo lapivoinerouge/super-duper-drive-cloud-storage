9e15223e364f46686cb66f4113e207df2f5ad450
lapivoinerouge <wioladys@gmail.com>
2020-07-25 21:00:08 +0200

    add methods in UserMapper

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
@@ -1,17 +1,25 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
 import com.udacity.jwdnd.course1.cloudstorage.entity.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
 public interface UserMapper {
+    @Select("SELECT * FROM USERS")
+    List<User> getUsers();
+
     @Select("SELECT * FROM USERS WHERE username = #{username}")
     User getUser(String username);
 
     @Insert("INSERT INTO USERS (username, salt, password, firstname, lastname) VALUES(#{username}, #{salt}, #{password}, #{firstName}, #{lastName})")
     @Options(useGeneratedKeys = true, keyProperty = "userId")
-    int insert(User user);
+    int insertUser(User user);
+
+    @Update("UPDATE USERS SET username=#{username}, firstname=#{firstName}, lastname=#{lastName} WHERE userId=#{userId}")
+    void updateUser(User user);
+
+    @Delete("DELETE FROM USERS WHERE userId=#{userId}")
+    void deleteUser(Integer userId);
 }
